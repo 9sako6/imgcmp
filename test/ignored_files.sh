1bb1279519b49eaf4553d50f3efcf35bc37bbc00
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-12-25 07:37:02 +0900

    fix test/*.sh

--- test/ignored_files.sh
+++ test/ignored_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 test/build.sh
-docker run -it -e DEBUG_IMGCMP=1 -e GITHUB_ACTOR=$(git config --global user.name) -e GITHUB_EMAIL=$(git config --global user.email) -e IGNORED_FILES=figs/logo* --name imgcmp 9sako6:imgcmp
+docker run -it -e DEBUG_IMGCMP=1 -e GITHUB_ACTOR=$(git config --global user.name) -e GITHUB_EMAIL=$(git config --global user.email) -e IGNORED_FILES=figs/logo* --name imgcmp imgcmp:debug
 test/remove.sh
