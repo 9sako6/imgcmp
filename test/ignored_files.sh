ad8fddc80e6b3be6316b675d940c775906f75a28
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-02-01 22:20:24 +0900

    Use Personal access token instead of GITHUB_TOKEN

--- test/ignored_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-test/build.sh
-docker run -it -e DEBUG_IMGCMP=1 -e GITHUB_ACTOR=$(git config --global user.name) -e GITHUB_EMAIL=$(git config --global user.email) -e IGNORED_FILES=figs/logo* --name imgcmp imgcmp:debug
-test/remove.sh
